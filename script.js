dbb6b147f12d25df8e114080b44fc8ffe6e4f582
Pranav <pd2772001@gmail.com>
2023-01-12 19:50:12 +0530

    odd even

---
+++ script.js
@@ -0,0 +1,15 @@
+const a = 2;
+const b = 45;
+
+function oddEven(a){
+    if (a % 2==0) {
+        console.log(a+' '+'is Even');
+    }
+    else
+    {
+        console.log(a+' '+'is Odd');
+    }
+}
+
+oddEven(a);
+oddEven(b);
